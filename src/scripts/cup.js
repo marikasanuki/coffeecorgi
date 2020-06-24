9e222ee1b81f96a4fb9866a0b57e4ddc710a3a37
marikasanuki <mari.kasanuki@gmail.com>
2020-06-24 10:06:59 -0400

    cup styling update pre-draw functions

--- src/scripts/cup.js
+++ src/scripts/cup.js
@@ -5,16 +5,15 @@ class Cup {
         this.canvasEle = canvasEle;
 
         ctx.translate(130, 10);
-        ctx.lineWidth = 1.5;
-        ctx.fillStyle = "rgb(58, 24, 24)";
 
         // 	top inner black ring
         ctx.save();
-        ctx.translate(0, 0);
+        // ctx.translate(0, 0);
         ctx.scale(2, 0.4);
-        // ctx.lineWidth = 1.5;
+        ctx.lineWidth = 1.5;
         ctx.beginPath();
         ctx.arc(0, 0, 18, 0, 2 * Math.PI, false);
+        ctx.fillStyle = "rgb(58, 24, 24)";
         ctx.fill();
         ctx.stroke();
         ctx.restore();
@@ -23,7 +22,7 @@ class Cup {
         ctx.save();
         ctx.translate(0, 0);
         ctx.scale(2, 0.4);
-        // ctx.lineWidth = 1.5;
+        ctx.lineWidth = 3.5;
         ctx.beginPath();
         ctx.arc(0, 0, 25, 0, 2 * Math.PI, false);
         ctx.stroke();
@@ -33,18 +32,9 @@ class Cup {
         ctx.save();
         ctx.translate(0, 10);
         ctx.scale(0.8, 0.15);
+        ctx.lineWidth = 1;
         ctx.beginPath();
         ctx.arc(0, 0, 50, 0, 1 * Math.PI, false);
-        ctx.stroke();
-        ctx.restore();
-
-        //bottom black ring
-        ctx.save();
-        ctx.translate(0, 59);
-        ctx.scale(1, 0.2);
-        ctx.lineWidth = 5;
-        ctx.beginPath();
-        ctx.arc(0, 0, 20, 0, 1 * Math.PI, false);
         ctx.stroke();
         ctx.restore();
 
@@ -52,16 +42,30 @@ class Cup {
         ctx.beginPath();
         ctx.moveTo(-50, 0);
         ctx.arcTo(-45, 45, 70, 115, 60);
-        ctx.lineWidth = 2;
+        ctx.lineWidth = 5;
         ctx.stroke();
 
         // 	right line of cup
         ctx.beginPath();
         ctx.moveTo(50, 0);
         ctx.arcTo(45, 43, 28, 55, 69);
-        ctx.lineWidth = 2;
+        ctx.lineWidth = 5;
         ctx.stroke();
         ctx.restore();
+
+        //bottom black ring
+        ctx.save();
+        ctx.translate(0, 59);
+        ctx.scale(1, 0.2);
+        ctx.lineWidth = 12;
+        ctx.beginPath();
+        ctx.arc(0, 0, 21, 0, 1 * Math.PI, false);
+        ctx.stroke();
+        ctx.restore();
+    }
+
+    drawCup() {
+
     }
 
     createCupIndex() {
