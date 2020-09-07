37eba63dea78f269a06b91dff0285a19bc4ddd2b
marikasanuki <mari.kasanuki@gmail.com>
2020-09-06 21:19:52 -0400

    footer with profile links

--- src/index.js
+++ src/index.js
@@ -7,11 +7,15 @@ import Cafe from "./scripts/cafe";
 document.addEventListener("DOMContentLoaded", function () {
 
     const modal = document.getElementById("modal");
-    const closeModalBtn = document.getElementById("close-modal-btn");
+    const closeModalBtn = document.getElementById("close-modal-icon");
     closeModalBtn.onclick = () => {
       modal.style.display = "none"; //hide modal from document
     };
 
+    const openModalBtn = document.getElementById("open-modal-icon");
+    openModalBtn.onclick = () => {
+        modal.style.display = "block";
+    };
 
     const canvasEle = document.getElementById('cc-cafe');
     canvasEle.width = 1300;
